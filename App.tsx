1c9c6cadc397f5a93eba35a488a7a9d073929f90
MiVitikk <mikko.vitikka@myy.haaga-helia.fi>
2026-09-01 20:04:17 +0300

    Added calculator history feature to the app.

--- App.tsx
+++ App.tsx
@@ -1,22 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
 import { useState } from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   const [num1, setNum1] = useState('')
   const [num2, setNum2] = useState('')
   
-  const [answer, setAnswer] = useState<number | null>( null );
-  
+  const [answer, setAnswer] = useState('');
+  const [history, setHistory] = useState<string[]>([]);
   const add = () => {
-    setAnswer(parseFloat(num1) + parseFloat(num2))
+    const n1 = parseFloat(num1)
+    const n2 = parseFloat(num2)
+    let result
+    result = n1 + n2
+    setAnswer(result.toString())
     clearInputs()
-    
+    setHistory(prev => [...prev, `${num1} + ${num2} = ${result}`]);
   }
 
   const minus = () => {
-    setAnswer(parseFloat(num1) - parseFloat(num2))
+    const n1 = parseFloat(num1)
+    const n2 = parseFloat(num2)
+    let result
+    result = n1 - n2
+    setAnswer(result.toString())
     clearInputs()
+    setHistory(prev => [...prev, `${num1} - ${num2} = ${result}`]);
   }
 
   const clearInputs = () => {
@@ -25,11 +35,11 @@ export default function App() {
   } 
 
   return (
-    <View style={styles.container}>
-
-      <Text style={styles.answer}>Result: {answer}</Text>
-      <StatusBar style="auto" />
-      <View style={{ flexDirection: 'column' }}>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+          <Text style={styles.answer}>Result: {answer}</Text>
+          <StatusBar style="auto" />
+          <View style={{ flexDirection: 'column' }}>
         <TextInput
           style={styles.input}
           onChangeText={num1 => (setNum1(num1))}
@@ -53,8 +63,15 @@ export default function App() {
         <Button onPress={add} title="+" />
         <Button onPress={minus} title="-" />
       </View>
+      <FlatList
+        data={history}
+        renderItem={({ item }) => <Text>{item}</Text>}
+        keyExtractor={(item) => item}
+      />
 
-    </View>
+    
+    </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -66,6 +83,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 50
 
   },
 
